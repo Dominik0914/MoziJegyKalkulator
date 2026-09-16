307f6a6d660687bac9af0c381fb2aada6891c92b
Dominik0914 <rozsa.dominik.2024d@telekimezotur.hu>
2026-09-16 12:14:38 +0200

    stringek megadasa és kis szamolas

--- MoziJegyKalkulator/Program.cs
+++ MoziJegyKalkulator/Program.cs
@@ -1,1 +1,15 @@
 ﻿string nev=Console.ReadLine();
+int kor = Convert.ToInt32(Console.ReadLine());
+bool diakigaolvany = false;
+int mozijegy = 2800;
+int popcorn = Convert.ToInt32(Console.ReadLine());
+int popcornar = 1500;
+int kedvezmeny = 0.20;
+
+int ossz= mozijegy + (popcorn * popcornar);
+
+
+if (kor < 18 ||diakigaolvany)
+{
+    ossz = (int)(ossz * (1 - kedvezmeny));
+}
