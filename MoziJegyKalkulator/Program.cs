8f295dd32c5e4e6ef3519db0b6f74eedbf4af392
Dominik0914 <rozsa.dominik.2024d@telekimezotur.hu>
2026-09-16 12:55:41 +0200

    vege

--- MoziJegyKalkulator/Program.cs
+++ MoziJegyKalkulator/Program.cs
@@ -8,14 +8,14 @@ if (diakigaolvanyInput == "igen")
     diakigaolvany = true;
 }
 else
-{ diakigaolvany = false;
+{ diakigaolvany = false; }
 int mozijegy = 2800;
 int mozijegyosz = Convert.ToInt32(Console.ReadLine());
 int popcorn = Convert.ToInt32(Console.ReadLine());
 int popcornar = 1500;
 double kedvezmeny = 0.20;
 int ujossz=0;
-int ossz= mozijegy + (popcorn * popcornar);
+int ossz= (mozijegy * mozijegyosz) + (popcorn * popcornar);
 ///3.feladat
     if (kor < 18 || diakigaolvany == true)
     {
@@ -30,6 +30,6 @@ Console.WriteLine($"rendelés: {mozijegyosz}db mozijegy + {popcorn}db popcorn");
 Console.WriteLine($"Alap összeg: {ossz} Ft");
 Console.WriteLine($"Fizetendő végösszeg: {ujossz} Ft");
 if (kor < 18 || diakigaolvany == true)
-    {
+{
         Console.WriteLine($"Státusz: {kedvezmeny * 100}% os kedvezmény érvényesítve");
-    }
+}
